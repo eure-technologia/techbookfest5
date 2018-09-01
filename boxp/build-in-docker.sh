59df12266cb4ccfbf53fb4443055b930b83fc459
boxp <tiyotiyouda@gmail.com>
2018-09-01 14:36:45 +0900

    Linux上でのパーミッション周り対応

--- boxp/build-in-docker.sh
+++ boxp/build-in-docker.sh
@@ -3,4 +3,5 @@
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
 # docker run -i -t -v $(pwd):/book vvakame/review /bin/bash
 
-docker run -t --rm -v $(pwd):/book vvakame/review:2.4 /bin/bash -ci "cd /book && ./setup.sh && npm run pdf"
+docker build -t boxp/review .
+docker run -t --rm -v $(pwd)/../:/book boxp/review /bin/bash -ci "cd /book/boxp && ./setup.sh && npm run pdf"
